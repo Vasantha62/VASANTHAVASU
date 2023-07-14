2585528de6e65b263b565ded1e0c5a6cb21c9b80
Vasantha62 <125265788+Vasantha62@users.noreply.github.com>
2023-07-14 14:10:52 +0530

    Update build.gradle

--- App.jsx
+++ App.jsx
@@ -1,67 +1,24 @@
-//import Child1 from "./components/Child1"
-//import Child2 from "./components/Child2"
-//import Childcls from "./components/Childcls"
-//import Child3 from "./components/Child3"
-//import Incre from "./components/Incre"
-//import Logo from "./components/Logo"//
-//import State from "./components/State"
-//import ClassProps from "./components/ClassProps"
-//import List from "./components/List"
-//import Statecls from "./components/Statecls"
-//import PropsChildren from "./components/PropChildren"
-//mport A from "./components/A"
-//import Lists from "./components/Lists"
-//import Style1 from "./components/Style1"
-//import Navbar from "./components/Navbar
-//import Navbar from "./components/Navbar"
-//import Navbar from "./components/Navbar"
-//import useState1 from "./components/Usestate1"
-//import Tcs from "./components/tcs.module.css"
-//import "./global.css"
-//import Style2 from "./components/Style2"
-//import Style3 from "./components/Style3"
-//import Inline from "./components/Inline"
-//import Img from "./components/Img"
+// build.gradle (app-level)
+dependencies {
+    // Retrofit
+    implementation 'com.squareup.retrofit2:retrofit:2.9.0'
+    implementation 'com.squareup.retrofit2:converter-gson:2.9.0'
+    implementation 'com.squareup.okhttp3:logging-interceptor:4.9.0'
 
-//import Main from "./Hooks/Main"//
+    // Room
+    implementation 'androidx.room:room-runtime:2.3.0'
+    kapt 'androidx.room:room-compiler:2.3.0'
 
-//import Controlled from "./components/Controlled"
-//import Amazon from "./components/Amazon"
-//import Navbar from "./components/Navbar"
-//import Tcs from "./components/Tcs"
-//import Navbar from "./components/Navbar"
-//import Zee5 from "./components/Zee5"
-//import Navbar from "./components/Navbar"
-//import Platform from "./components/Plaftform"
-//import Godaddy from "./components/Godaddy"
-//import Useeffect from "./Hooks/Useeffect"
-//import X from "./Hooks/X"
-//import {createContext} from "react"
+    // RecyclerView
+    implementation 'androidx.recyclerview:recyclerview:1.2.1'
 
+    // Material Design
+    implementation 'com.google.android.material:material:1.4.0'
 
-//import Uncontrolled from "./components/Uncontrolled";
-//import Xml from "./components/Xml"
- //export let userData=createContext()
- //import React, { useState } from "react";
- import React, { useState, useEffect } from 'react';
- import UserDashboard from './UserDashboard';
- 
- const App = () => {
-   const [savedProperties, setSavedProperties] = useState([]);
-   const [contactHistory, setContactHistory] = useState([]);
- 
-   useEffect(() => {
-     // Fetch saved properties and set state
-     // Fetch contact history and set state
-   }, []);
- 
-   return (
-     <div>
-       <h1>My Real Estate App</h1>
-       <UserDashboard savedProperties={savedProperties} contactHistory={contactHistory} />
-     </div>
-   );
- };
- 
- export default App;
- 
+    // Coroutines
+    implementation 'org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0'
+
+    // Glide (for image loading)
+    implementation 'com.github.bumptech.glide:glide:4.12.0'
+    kapt 'com.github.bumptech.glide:compiler:4.12.0'
+}
